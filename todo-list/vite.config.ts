439a43abcb1d7e44595eafe993de4bea3fe8feb3
Nerion-1337 <nerion.1337.dev@gmail.com>
2025-11-19 23:21:08 +0100

    add all variable sans prefixe

--- todo-list/vite.config.ts
+++ todo-list/vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  base: (import.meta as any).env.VITE_ASSETS,
-})
+export default ({ mode }: { mode: string }) => {
+  // Charger le fichier .env de manière explicite
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react()],
+    base: env.VITE_ASSETS || '/', // Lecture correcte de la variable
+  });
+};
+
